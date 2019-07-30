b11bea10ed74b5cfa28971e2f9fb0b84bbd90218
Miao <tymiao@gwmail.gwu.edu>
2019-07-30 14:41:12 -0400

    clean up

--- plugin_tests/create_histogram_test.py
+++ plugin_tests/create_histogram_test.py
@@ -1,21 +1,21 @@
-# import unittest
+import unittest
 
-# from girder.plugins.histogram.create_histogram import computeHistogram
+import histogramScript.create_histogram
 
 
-# class CreateHistogramTest(unittest.TestCase):
-#     def setUp(self):
-#         self.path = 'plugins/histogram/plugin_tests/test_files/Seg.tiff'
-#         self.label = False
-#         self.bins = 256
-#         self.bitmask = False
+class CreateHistogramTest(unittest.TestCase):
+    def setUp(self):
+        self.path = 'plugins/histogram/plugin_tests/test_files/Seg.tiff'
+        self.label = False
+        self.bins = 256
+        self.bitmask = False
 
-#     def testComputeHistogram(self):
+    def testComputeHistogram(self):
 
-#         hist, binEdges = computeHistogram(self.path,
-#                                           self.label,
-#                                           self.bins,
-#                                           self.bitmask)
-#         self.assertEqual(binEdges[-1], 256)
-#         self.assertEqual(len(binEdges), 257)
-#         self.assertEqual(hist[-1], 5647232)
+        hist, binEdges = histogramScript.create_histogram.computeHistogram(self.path,
+                                                                           self.label,
+                                                                           self.bins,
+                                                                           self.bitmask)
+        self.assertEqual(binEdges[-1], 256)
+        self.assertEqual(len(binEdges), 257)
+        self.assertEqual(hist[-1], 5647232)
