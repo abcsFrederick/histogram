a17a1226bfe1a65c241668234bf3abfde7652554
Miao <tymiao@gwmail.gwu.edu>
2019-09-03 15:28:38 -0400

    download all

--- plugin.cmake
+++ plugin.cmake
@@ -1,21 +1,19 @@
-# add_standard_plugin_tests()
-add_python_style_test(
-  python_static_analysis_histogram_tests
-  "${PROJECT_SOURCE_DIR}/plugins/histogram/plugin_tests"
-)
+add_standard_plugin_tests()
+# add_python_style_test(
+#   python_static_analysis_histogram_tests
+#   "${PROJECT_SOURCE_DIR}/plugins/histogram/plugin_tests"
+# )
 
-add_python_test(histogram
-  PLUGIN histogram
-)
+# add_python_test(histogram
+#   PLUGIN histogram
+# )
 
-add_eslint_test(
-  js_static_analysis_histogram_tests "${PROJECT_SOURCE_DIR}/plugins/histogram"
-)
+# add_eslint_test(
+#   js_static_analysis_histogram_tests "${PROJECT_SOURCE_DIR}/plugins/histogram"
+# )
 
-add_web_client_test(
-  histogram_test
-  "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/histogramSpec.js"
-  PLUGIN histogram
-  TEST_MODULE "plugin_tests.web_client_test"
-  TEST_PYTHONPATH "${CMAKE_CURRENT_LIST_DIR}"
-)
+# add_web_client_test(
+#   histogram_test
+#   "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/histogramSpec.js"
+#   PLUGIN histogram
+# )
