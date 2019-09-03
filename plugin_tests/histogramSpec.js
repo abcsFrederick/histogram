1f1ab647ffd27ee5f2fa83a53dda187e3070bd91
Miao <tymiao@gwmail.gwu.edu>
2019-09-03 11:29:24 -0400

    s3 support

--- plugin_tests/histogramSpec.js
+++ plugin_tests/histogramSpec.js
@@ -54,14 +54,11 @@ $(function () {
             });
 
             waitsFor(function () {
+            //     console.log(window.girder.plugins.large_image);
                 console.log('-------------modal-------------');
                 console.log($('.modal'));
                 console.log('-------------modal bs.modal-------------');
                 console.log($('.modal').data('bs.modal'));
-                console.log('-------------modal bs.modal isShown-------------');
-                console.log($('.modal').data('bs.modal').isShown);
-                console.log('-------------length-------------');
-                console.log($('#g-dialog-container:visible').length);
                 return $('.modal').data('bs.modal') &&
                     $('.modal').data('bs.modal').isShown === true &&
                     $('#g-dialog-container:visible').length > 0;
